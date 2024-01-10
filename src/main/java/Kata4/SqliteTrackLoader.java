f8e4fea00b512f38176c208fea02304f13af6cc0
Mario <mario2002pc@hotmail.com>
2024-01-10 16:01:51 +0000

    Creation of classes Track and TrackLoader

---
+++ src/main/java/Kata4/SqliteTrackLoader.java
@@ -0,0 +1,4 @@
+package Kata4;
+
+public class SqliteTrackLoader {
+}
---
+++ src/main/java/Kata4/Track.java
@@ -0,0 +1,3 @@
+package Kata4;
+
+public record Track(String track, int milliseconds, double unitPrice, String album, String genre, String artist) {}
