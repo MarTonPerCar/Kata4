bfe5971730cbd00d7edbd81727d8958e49319839
Mario <mario2002pc@hotmail.com>
2024-01-10 16:10:35 +0000

    Creation of Main and Finish

--- src/main/java/Kata4/Main.java
+++ src/main/java/Kata4/Main.java
@@ -1,4 +1,16 @@
 package Kata4;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
+    public static void main (String[] args) throws SQLException {
+        try (Connection connection = DriverManager.getConnection("jdbc:sqlite:chinook.db")){
+            SqliteTrackLoader Tabla = new SqliteTrackLoader(connection);
+            for (Track track : Tabla.loadsql()) {
+                System.out.println(track);
+            }
+        }
+    }
 }
